ddb1162fed4d95b0c5105728bd6d5e1e9152157b
xinyunkang <xinyunkang@gmail.com>
2017-10-20 15:02:41 -0500

    Initial website

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -1,20 +1,26 @@
-'use strict';
-var http = require('http');
+var http = require("http");
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 3000;
 //var ejsEngine = require("ejs-locals");
-//setup the view engine
+var controllers = require("./controllers");
+
+// Setup the View Engine
 //app.set("view engine", "jade");
-//app.engine("ejs", ejsEngine); //support master pages
-//app.set("view engine", "ejs");
+//app.engine("ejs", ejsEngine); // support master pages
+//app.set("view engine", "ejs"); // ejs view engine
 app.set("view engine", "vash");
 
-//set the public static resource folder
+// set the public static resource folder
 app.use(express.static(__dirname + "/public"));
 
-//map the routes
+// Map the routes
 controllers.init(app);
 
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({ name: "Shawn", isValid: true, group: "Admin" });
+});
+
 var server = http.createServer(app);
-server.listen(port);
+
+server.listen(3000);
